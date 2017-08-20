1784728244aa43d7d2579d93a463a760d28b04b2
lakssman <31154308+lakssman@users.noreply.github.com>
2017-08-20 15:35:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,21 @@ button.onclick = function () {
     request.open('GET', 'http://lakshman809.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+//Submit name
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    //Make request to server and send name
+    
+    //Capture list of names
+    var names = ['name1', 'name2', 'name3'];
+    var list = '';
+    for (var i=0; i<names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
