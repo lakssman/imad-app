942744d705a0a7b1d2e86dac5cd46bb6ac228089
lakssman <31154308+lakssman@users.noreply.github.com>
2017-08-20 10:14:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,10 +110,6 @@ app.get(':articleName', function (req,res) {
 }
 );
 
-app.get('/article-one', function (req,res) {
-  res.send(createTemplate(articleOne));
-});
-
 app.get('/article-two', function (req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
